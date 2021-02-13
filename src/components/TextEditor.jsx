77db44814c62d2846924c009f6d5dee9edd26d23
Yuldash Atajanov <cybergod1123@gmail.com>
2021-02-13 03:25:15 +0300

    добавлен функционал работы курсора и показ текста в реальном времени

--- src/components/TextEditor.jsx
+++ src/components/TextEditor.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import '../css/simulator.css';
+
 /*
     показывает исходный текст
     здесь должен находится: 
@@ -9,21 +11,65 @@ import ReactDOM from 'react-dom';
         зеленым будет помечатся набранный текст(тоже отдельный компонент)            
 */
 
-class TextEditor extends React.Component{
-    constructor(props){
+class TextEditor extends React.Component {
+    constructor(props) {
         super(props);
 
+        this.state = {
+            currentChar: '',//символ который нужно ввести(положение курсора)
+            prevChars: '',//введенные символы
+            nextChars: '',//не введенные символы
+        }
 
+        this.index = 0 //разделитель между введенным и не введенными символыми
+        this.onKeyHandle=this.onKeyHandle.bind(this);
     }
 
-    render(){
+    // метод вызывается при правильном вводе символа: переводит курсор на следующий символ
+    ifCorrectSymbol(value) {
+        this.setState({
+            prevChars: value.slice(0, this.index),
+            currentChar: value.charAt(this.index),
+            nextChars: value.slice(this.index + 1),
+            cursorClass: 'correct-cursor'
+        });
+        this.index++;
+    }
+
+    // обработка клавиатурного ввода-
+    onKeyHandle(event) {
+        console.log(event.key);
+        if (event.key === this.state.currentChar) {
+            this.ifCorrectSymbol(this.props.value);
+        } else if (event.key !== 'Shift') {
+            this.setState({
+                cursorClass: 'uncorrect-cursor'
+            })
+        }
+    }
+    componentWillReceiveProps(props){
+        this.ifCorrectSymbol(props.value); //обновшление текста
+    }
+
+    componentDidMount() {
+        document.onkeydown = this.onKeyHandle;//привязка обработчика клавиатурного ввода к дом
+
+    }   
+
+    render() {
+
+
 
         return (
-            <div>
-                {this.props.value}
+            <div className="text__editor">
+                <pre className="prev-chars">{this.state.prevChars}</pre>
+                <pre className={this.state.cursorClass}>{this.state.currentChar}</pre>
+                <pre className="next-chars">{this.state.nextChars}</pre>
             </div>
         )
     }
 }
 
 export default TextEditor;
+
+
--- src/components/TypingSimulator.jsx
+++ src/components/TypingSimulator.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 
-import '../css/tt-sim.css'
+import '../css/simulator.css'
 import TextEditor from './TextEditor';
 
 /*
@@ -19,12 +19,10 @@ class TypingSimulator extends React.Component {
         super(props);
 
         this.state = {
-            text: 'hello',//полученный текст
-            language: 'English'//язык текста
+            text: '',//полученный текст
+            language: 'English',//язык текста
         }
-        
     }
-
     /**
         генератор случайного текста
         параметры:
@@ -35,18 +33,17 @@ class TypingSimulator extends React.Component {
      */
     generateText(language) {
 
-        let number=(arguments[1])?arguments[1]:5;
+        let number = (arguments[1]) ? arguments[1] : 5;
 
         switch (language) {
             case 'English': {
-                let param="type=all-meat"+"&sentences="+number+"&start-with-lorem=1";
+                let param = "type=all-meat" + "&sentences=" + number + "&start-with-lorem=1";
 
                 // отправка запроса на сервер
-                axios.get('https://baconipsum.com/api/?'+param).then(result => {
-                    
+                axios.get('https://baconipsum.com/api/?' + param).then(result => {
                     // изменение текста 
                     this.setState({
-                        text: result.data.join()
+                        text: result.data.join(),
                     })
                     console.log("ответ с сервера получен");
                 })
@@ -59,29 +56,25 @@ class TypingSimulator extends React.Component {
                 // отправка запроса на сервер
                 axios.get('https://fish-text.ru/get?' + params).then(result => {
                     console.log("ответ с сервера получен");
-                    
                     // изменение текста 
                     this.setState({
-                        text: result.data.text
+                        text: result.data.text,
                     });
                 })
                 break;
             }
         }
     }
-
-
-
+    
 
     componentDidMount() {
-        this.generateText(this.state.language,1);
-    
+        this.generateText(this.state.language, 1); //генерация случайного текста
     }
 
     render() {
         return (
             <div className="simulator">
-                <TextEditor value={this.state.text}/>
+                <TextEditor onstart={this.onload} value={this.state.text} />
             </div>
         )
     }
